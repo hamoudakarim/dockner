67f2dac577f145e3ad5f7d8ac65447b8b7b27566
doudou <marzoukiwissem.mw@gmail.com>
2018-02-19 13:19:34 +0100

    another layout change

--- app/routes.py
+++ app/routes.py
@@ -4,27 +4,26 @@ from flask import render_template, url_for
 
 
 
-
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
     return(render_template("index.html"))
 
 @app.route("/containers")
 def containers():
-    return("Hello containers")
+    return(render_template("containers.html"))
 
 
 @app.route("/images")
 def images():
-    return("Hello images")
+    return(render_template("images.html"))
 
 
 @app.route("/settings")
 def settings():
-    return("Hello settings")
+    return(render_template("settings.html"))
 
 
 @app.route("/status")
 def status():
-    return("Hello status")
+    return(render_template("status.html"))
